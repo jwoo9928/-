1e461188dee37b218c27ccdf159848bb2c95f6de
Jaewoo Park <jaewoopark@Mac-mini.local>
2020-10-11 15:46:58 +0900

    week6

---
+++ week6/training_02.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+class Animal {
+private:
+	std::string name;
+
+public:
+	Animal() {};
+	Animal(std::string name) : name(name) {};
+
+	void showName() {
+		std::cout << "Name is " << name << std::endl;
+	}
+
+	//operator+ �߰��ϼ���
+	Animal operator+(Animal &ref) {
+		return Animal(name+ref.name);
+	}
+
+};
+
+int main() {
+	Animal cat("Nabi");
+	cat.showName();
+	Animal dog("Jindo");
+	dog.showName();
+
+	Animal catDog = dog + cat; //������ �ռ�
+	catDog.showName();
+
+	dog.showName();
+
+	getchar();
+	return 0;
+}
